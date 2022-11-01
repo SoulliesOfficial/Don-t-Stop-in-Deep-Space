77a58e9a2f0741969d7e443b528728c877c5886c
ruaruarua614 <yuyao6164@gmail.com>
2022-11-01 11:27:05 -0400

    audio manager pseudo code
    pseudo code

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -7,16 +7,21 @@ public class AudioManager : MonoBehaviour
     public AudioClip bgmSmooth;
     public AudioClip bgmFast;
     public float currentStage;//which room the player is in now(to change to corresponding bgm)
+    public AudioSource audioPlayer;
+    //use smth to get the current room the player is in
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioPlayer = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //check which room the player is in
+            //for room 0 & 1, use smooth
+
+            //for room 2 & 3, use fast
     }
 }
