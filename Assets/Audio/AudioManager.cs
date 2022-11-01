c775574255b7122fe9e85ec43e14b2299a719b70
ruaruarua614 <yuyao6164@gmail.com>
2022-11-01 11:19:21 -0400

    Audio Manager basic
    still working on the script

---
+++ Assets/Audio/AudioManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip bgmSmooth;
+    public AudioClip bgmFast;
+    public float currentStage;//which room the player is in now(to change to corresponding bgm)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
